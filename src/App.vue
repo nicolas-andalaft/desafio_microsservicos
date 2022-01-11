4121450f18282cd9fb2de7a73f42c21ae854737f
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2022-01-11 18:01:03 -0300

    pagina de login

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+	<nav>
+		<router-link to="/">Home</router-link> |
+		<router-link to="/login" v-if="!authenticated">Login</router-link>
+		<router-link to="/profile" v-if="authenticated"> Profile </router-link>
+		<a v-if="authenticated" v-on:click="logout()"> Logout </a>
+	</nav>
+	<div id="content">
+		<router-view />
+	</div>
 </template>
+
+<script>
+export default {
+	name: 'app',
+	data: () => {
+		rauthenticated: false;
+	},
+	async created() {
+		await this.isAuthenticated();
+		this.$auth.authStateManager.subscribe(this.isAuthenticated);
+	},
+	watch: {
+		// Everytime the route changes, check for auth status
+		$route: 'isAuthenticated',
+	},
+	methods: {
+		async isAuthenticated() {
+			this.authenticated = await this.$auth.isAuthenticated();
+		},
+		async logout() {
+			await this.$auth.signOut();
+		},
+	},
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+	font-weight: bold;
+	color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
