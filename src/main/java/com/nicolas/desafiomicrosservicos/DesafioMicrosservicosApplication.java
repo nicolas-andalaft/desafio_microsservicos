e69ba9e1a967610bb73b7dc2b78610db14799ecf
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2022-01-11 16:13:57 -0300

    Arquivos iniciais

---
+++ src/main/java/com/nicolas/desafiomicrosservicos/DesafioMicrosservicosApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.desafiomicrosservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMicrosservicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMicrosservicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolas/desafiomicrosservicos/DesafioMicrosservicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.desafiomicrosservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMicrosservicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
