bb84d7f84e31d4e41b8055fb46719d2154b2eaa4
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2022-01-13 11:20:00 -0300

    API passada para outro repositório

--- src/main/java/com/nicolas/desafiomicrosservicos/DesafioMicrosservicosApplication.java
+++ src/main/java/com/nicolas/desafiomicrosservicos/DesafioMicrosservicosApplication.java
@@ -2,27 +2,11 @@ package com.nicolas.desafiomicrosservicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class DesafioMicrosservicosApplication {
 
 	public static void main(String[] args) {
-        new StockAPI();
 		SpringApplication.run(DesafioMicrosservicosApplication.class, args);
 	}
-
-	@Configuration
-    @EnableWebSecurity
-    class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.cors().and()
-                    .authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
-                    .oauth2ResourceServer().jwt();
-        }
-    }
 }
