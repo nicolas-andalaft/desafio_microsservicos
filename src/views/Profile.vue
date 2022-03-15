69e72b269565e68993b145e50eda8b493b450cd2
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2022-03-15 12:51:52 -0300

    Componente de lista de cards usado na página de perfil

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,49 +1,34 @@
 <template>
 	<div>
-		<h2>Stock ballance</h2>
-		<p v-if="stocksList.length === 0">You don't have any stocks</p>
-		<n-grid
-			cols="1 s:2 m:3 l:4 xl:5 xxl:6"
-			responsive="screen"
-			x-gap="10"
-			y-gap="10"
-		>
-			<n-grid-item v-for="(stock, index) in stocksList" v-bind:key="index">
-				<stock-card :stock="stock" />
-			</n-grid-item>
-		</n-grid>
-		<h2>Orders</h2>
-		<p v-if="orderList.length === 0">You don't have any orders</p>
-		<n-grid
-			cols="1 s:2 m:3 l:4 xl:5 xxl:6"
-			responsive="screen"
-			x-gap="10"
-			y-gap="10"
-		>
-			<n-grid-item v-for="(order, index) in orderList" v-bind:key="index">
-				<order-card :order="order" />
-			</n-grid-item>
-		</n-grid>
+		<card-list
+			title="Stock ballance"
+			:list="stocksList"
+			:card_component="StockCard"
+		/>
+		<card-list title="Orders" :list="orderList" :card_component="OrderCard" />
 	</div>
 </template>
 
 <script>
-import { NGrid, NGridItem } from 'naive-ui';
+import CardList from '@/components/cardList';
 import StockCard from '@/components/stockCard';
 import OrderCard from '@/components/orderCard';
 import OrdersController from '@/controllers/OrdersController';
 
 export default {
 	components: {
-		StockCard,
-		OrderCard,
-		NGrid,
-		NGridItem,
+		CardList,
 	},
 	data() {
 		return {
 			stocksList: [],
 			orderList: [],
+		};
+	},
+	setup() {
+		return {
+			StockCard,
+			OrderCard,
 		};
 	},
 	beforeMount() {
